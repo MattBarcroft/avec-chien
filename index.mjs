1f255a85929bfebd493749c89d6f36464150bbae
Matt Barcroft <matthew.barcroft@skybettingandgaming.com>
2025-06-20 15:04:51 +0100

    initial commit

---
+++ index.mjs
@@ -0,0 +1,86 @@
+import axios from "axios";
+
+export const handler = async (event) => {
+  const CLIENT_ID = "xxx";
+  const CLIENT_SECRET = "xxx";
+  const ACCESS_TOKEN = "xxx";
+  const CODE = "xxx";
+  const STRAVA_API_BASE = "https://www.strava.com/api/v3";
+
+  const getAthlete = async () => {
+    const getRecentActivityIds = async () => {
+      console.log("hi");
+      try {
+        const response = await axios.get(
+          `${STRAVA_API_BASE}/athlete/activities?after=1735689600&per_page=100`,
+          {
+            headers: {
+              Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+          }
+        );
+        return response.data.map((activity) => activity.id);
+      } catch (error) {
+        console.error("Error fetching activities:", error);
+      }
+    };
+
+    const filterActivities = async (activityIds) => {
+      const dogActivities = [];
+
+      for (const id of activityIds) {
+        const response = await axios.get(
+          `${STRAVA_API_BASE}/activities/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+          }
+        );
+        if (response.data.description?.includes("🐾")) {
+          dogActivities.push({
+            id: response.data.id,
+            distance: response.data.distance,
+            description: response.data.description,
+          });
+        }
+      }
+
+      return dogActivities;
+    };
+    // const updateDescription = async (id, updatedDescription) => {
+    //   const response = await axios.put(
+    //     `${STRAVA_API_BASE}/activities/${id}`,
+    //     {
+    //       description: updatedDescription,
+    //     },
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${ACCESS_TOKEN}`,
+    //       },
+    //     }
+    //   );
+    //   console.log(response.data);
+    // };
+
+    const activityIds = await getRecentActivityIds();
+    const dogActivities = await filterActivities(activityIds);
+
+    const totalDistance = (
+      dogActivities.reduce((total, activity) => total + activity.distance, 0) /
+      1000
+    ).toFixed(1);
+    const latestActivityId = dogActivities[dogActivities.length - 1].id;
+    const latestActivityDescription =
+      dogActivities[dogActivities.length - 1].description;
+
+    const updatedDescription = `${latestActivityDescription.replace(
+      "🐾",
+      ""
+    )} | 🐾🐕 YTD = ${totalDistance}km`;
+    console.log(latestActivityId, updatedDescription);
+
+    // await updateDescription(latestActivityId, updatedDescription);
+  };
+  getAthlete();
+};
